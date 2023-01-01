013417567f9eda918ed4778dfef37a8da388c5dc
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2023-01-01 17:53:36 +0000

    Upgrading the smart contracts programatically

--- deploy/01-deploy-box.js
+++ deploy/01-deploy-box.js
@@ -6,12 +6,15 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy, log } = deployments
   const { deployer } = await getNamedAccounts()
 
+    const waitBlockConfirmations = developmentChains.includes(network.name)
+        ? 1
+        : VERIFICATION_BLOCK_CONFIRMATIONS
   log('----------------------------')
   const box = await deploy('Box', {
     from: deployer,
     args: [],
     log: true,
-    waitConfirmations: network.config.blockConfirmations,
+        waitConfirmations: waitBlockConfirmations,
     proxy: {
       proxyContract: 'OpenZeppelinTransparentProxy',
       viaAdminContract: {
@@ -20,8 +23,11 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
       },
     },
   })
+
   if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
     log('Verifying................')
-    await verify(box.address, [])
+    const boxAddress = (await ethers.getContract('Box_Implementation')).address
+    await verify(boxAddress, arguments)
   }
 }
+module.exports.tags = ["all", "box"]
